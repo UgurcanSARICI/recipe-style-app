6ad9ac8fa8abc66191ac974f92292fb73d073acb
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-31 20:28:49 +0300

    Routing-GlobalStyle

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import AppRouter from "./router/AppRouter";
+
 import "./App.css";
 
 function App() {
-  return <div className="App">RECIPE APP</div>;
+  return (
+    <div className="App">
+      <AppRouter />
+    </div>
+  );
 }
 
 export default App;
