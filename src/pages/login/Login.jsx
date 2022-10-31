c24df6cd5bd9fcc7fec00bd1f4c1bc849927ab6b
UgurcanSARICI <ugurcansarici@gmail.com>
2022-10-31 22:19:24 +0300

    Login

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
+import {
+  FormContainer,
+  Header,
+  LoginContainer,
+  StyledButton,
+  StyledForm,
+  StyledImg,
+  StyledInput,
+} from "./Login.style";
+import meal from "../../assets/meal.svg";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate();
+  const userInfo = {
+    username: "Uğurcan",
+  };
 
-export default Login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sessionStorage.setItem("user", JSON.stringify(userInfo));
+    navigate(-1);
+  };
+
+  return (
+    <LoginContainer>
+      <FormContainer>
+        <StyledImg src={meal} />
+        <Header>Recipe</Header>
+        <StyledForm onSubmit={handleSubmit}>
+          <StyledInput placeholder="User Name" type="text" />
+          <StyledInput placeholder="Password" type="password" />
+          <StyledButton type="submit">Login</StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
+
+export default Login;
