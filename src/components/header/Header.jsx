67fc9232ab666a4815cf1dfdf8ba91d0eda8af65
UgurcanSARICI <ugurcansarici@gmail.com>
2022-11-01 21:01:57 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Header.style";
+import {
+  Button,
+  FoodInput,
+  FormContainer,
+  HeaderContainer,
+  MainHeader,
+  Select,
+} from "./Header.style";
 
-const Header = () => {
+const Header = ({ setQuery, setSelectedMeal, mealType }) => {
+  console.log(mealType);
   return (
-    <div>
-      Header
-    </div>
-  )
-}
+    <HeaderContainer>
+      <MainHeader>Recipe App</MainHeader>
 
-export default Header
+      <FormContainer>
+        <FoodInput
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        <Button type="submit">SEARCH</Button>
+
+        <Select
+          name="mealType"
+          id="mealType"
+          onChange={(e) => setSelectedMeal(e.target.value)}
+        >
+          {mealType.map((meal, index) => {
+            return (
+              <option key={index} value={meal}>
+                {meal}
+              </option>
+            );
+          })}
+        </Select>
+      </FormContainer>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
